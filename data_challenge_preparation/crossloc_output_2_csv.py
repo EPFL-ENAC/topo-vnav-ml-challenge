5ecee4ee945c86bd58bd0b5eb6d0a85b8835d9e9
Régis Lonagchamp <regis@lonco.ch>
2022-06-09 10:46:36 +0200

    add uncertainty to the csv

--- data_challenge_preparation/crossloc_output_2_csv.py
+++ data_challenge_preparation/crossloc_output_2_csv.py
@@ -91,10 +91,24 @@ def crossloc_res_unzip_get_npy_paths(result_folder: str, file_name: str = 'pose_
     return list_of_npy_paths
 
 
-def numpy_array_2_csv(path_poses: [str, list], path_out_csv: str,
+def get_dupose_u_value(path_file_pred_r_or_t_error : str,i : int):
+    """
+    Get the rotation or translation Umodel applied from dupose
+    :param path_file_pred_r_or_t_error:
+    :param i:
+    :return:
+    """
+    data = np.load(path_file_pred_r_or_t_error)
+    return data[i]
+
+
+
+def numpy_array_2_csv(path_poses: [str, list], path_out_csv: str, dupose_result_folder : str = None,
                                   origin_of_local_coordinate_system_x: float = 0,
                                   origin_of_local_coordinate_system_y: float = 0,
-                                  origin_of_local_coordinate_system_z: float = 0):
+                                  origin_of_local_coordinate_system_z: float = 0,
+                                  uncertainty_val_translation: float = 0,
+                                uncertainty_val_rotation: float = 0):
     """
     Convert positions and angles from the pose into WGS84 and ° and write a result csv
     :param path_poses: path or list of paths of the npy pose files
@@ -112,7 +126,21 @@ def numpy_array_2_csv(path_poses: [str, list], path_out_csv: str,
     else:
         list_path_pose = path_poses
 
-    for npy_path_file in list_path_pose:
+    for i, npy_path_file in enumerate(list_path_pose[:1]):
+
+
+        if dupose_result_folder :
+            u_model_translation_file_path = os.path.join(dupose_result_folder,'pred_t_error.npy')
+            u_model_translation_value = get_dupose_u_value(u_model_translation_file_path,i)
+
+            u_model_rotation_file_path = os.path.join(dupose_result_folder,'pred_r_error.npy')
+            u_model_rotation_value = get_dupose_u_value(u_model_rotation_file_path,i)
+        else :
+            u_model_translation_value = uncertainty_val_translation
+            u_model_rotation_value = uncertainty_val_rotation
+
+
+
         npy_data = numpy_loader(npy_path_file)
         coordinates = npy_data[0:3, 3]
         # row_csv_file.append(list(coordinates))
@@ -120,7 +148,8 @@ def numpy_array_2_csv(path_poses: [str, list], path_out_csv: str,
                                                                                origin_of_local_coordinate_system_x,
                                                                                origin_of_local_coordinate_system_y,
                                                                                origin_of_local_coordinate_system_z)
-        current_row = [lat, lng, alt, azimuth, tilt, roll]
+
+        current_row = [lat, lng, alt, azimuth, tilt, roll, u_model_translation_value,u_model_rotation_value]
 
         row_csv_file.append(current_row)
 
@@ -131,10 +160,14 @@ def numpy_array_2_csv(path_poses: [str, list], path_out_csv: str,
 
 
 
-def create_csv_for_codalab_submission(crossloc_pose_result_folder : str, pose_file_name : str,  path_file_csv : str,
+def create_csv_for_codalab_submission(crossloc_pose_result_folder : str, pose_file_name : str,
+                                      dupose_result_folder : str,
+                                      path_file_csv : str,
                                       origin_of_local_coordinate_system_x : float,
                                       origin_of_local_coordinate_system_y : float,
-                                      origin_of_local_coordinate_system_z : float) :
+                                      origin_of_local_coordinate_system_z : float,
+                                      uncertainty_val_translation: float,
+                                      uncertainty_val_rotation: float) :
     """
     Run the full translation from Crossloc results files to a CSV file that can be loaded in the codalab competition
     :param crossloc_pose_result_folder: Path of the folder that contain the Crossloc result (with --save_pred).
@@ -150,8 +183,9 @@ def create_csv_for_codalab_submission(crossloc_pose_result_folder : str, pose_fi
     list_of_npy_paths = crossloc_res_unzip_get_npy_paths(crossloc_pose_result_folder, pose_file_name)
 
     # Create prediction CSV based on the Crossloc results (previously unzip)
-    numpy_array_2_csv(list_of_npy_paths, path_file_csv, origin_of_local_coordinate_system_x,
-                      origin_of_local_coordinate_system_y, origin_of_local_coordinate_system_z)
+    numpy_array_2_csv(list_of_npy_paths, path_file_csv, dupose_result_folder, origin_of_local_coordinate_system_x,
+                      origin_of_local_coordinate_system_y, origin_of_local_coordinate_system_z,
+                      uncertainty_val_translation,uncertainty_val_rotation)
 
 
 def _config_parser():
@@ -194,29 +228,31 @@ if __name__ == '__main__':
     origin_of_local_coordinate_system_x = 6.5668
     origin_of_local_coordinate_system_y = 46.5191
     origin_of_local_coordinate_system_z = 390
+    uncertainty_val_translation = 0.22
+    uncertainty_val_rotation = 1
     crossloc_pose_result_folder = 'C:\\projects\\vnav\\CrossLoc\\weight\\coord_pred_ckpt_iter_crossloc_naturescape_in-place.net_test_drone_real'
+    dupose_result_folder = None
     path_file_csv = 'C:\\projects\\vnav\\CrossLoc\\weight\\gt.csv'
 
-    create_csv_for_codalab_submission(crossloc_pose_result_folder, 'pose_gt.npy', path_file_csv,
+    create_csv_for_codalab_submission(crossloc_pose_result_folder, 'pose_gt.npy',dupose_result_folder, path_file_csv,
                                       origin_of_local_coordinate_system_x,
                                       origin_of_local_coordinate_system_y,
-                                      origin_of_local_coordinate_system_z)
+                                      origin_of_local_coordinate_system_z,
+                                      uncertainty_val_translation,uncertainty_val_rotation)
 
 
-    # # Run crossloc with the parameter --save_pred to get all the poses.
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    # # Unzip Crossloc results and get the list of all pose_gt.npy
-    # list_of_npy_paths = crossloc_res_unzip_get_npy_paths(crossloc_pose_result_folder, 'pose_gt.npy')
-    #
-    # # Create ground truth CSV based on the Crossloc results (previously unzip)
-    # ai_competition_result_file = 'C:\\projects\\vnav\\CrossLoc\\weight\\gt.csv'
-    # numpy_array_2_csv(list_of_npy_paths, ai_competition_result_file,
-    #                               origin_of_local_coordinate_system_x, origin_of_local_coordinate_system_y,
-    #                               origin_of_local_coordinate_system_z)
+    # Run crossloc with the parameter --save_pred to get all the poses.
+
+
+    # Unzip Crossloc results and get the list of all pose_gt.npy
+    list_of_npy_paths = crossloc_res_unzip_get_npy_paths(crossloc_pose_result_folder, 'pose_gt.npy')
+
+    # Create ground truth CSV based on the Crossloc results (previously unzip)
+    dupose_result_folder = "C:\\projects\\vnav\\data\\datasets_dupose_full_dataset_urbanscape_in_place_MLE_testing_result\\urbanscape_in_place_MLE_testing_result"
+    path_file_csv = 'C:\\projects\\vnav\\CrossLoc\\weight\\est.csv'
+
+    create_csv_for_codalab_submission(crossloc_pose_result_folder, 'pose_pred.npy', dupose_result_folder, path_file_csv,
+                                      origin_of_local_coordinate_system_x,
+                                      origin_of_local_coordinate_system_y,
+                                      origin_of_local_coordinate_system_z,
+                                      uncertainty_val_translation, uncertainty_val_rotation)
