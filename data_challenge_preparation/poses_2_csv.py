5b38e19a3f3fac01c7c637eb374a55738c298c48
Régis Lonagchamp <regis@lonco.ch>
2022-06-16 08:59:16 +0200

    refactor

--- data_challenge_preparation/poses_2_csv.py
+++ data_challenge_preparation/poses_2_csv.py
@@ -1,8 +1,9 @@
 """
 This file reformat the outputs of the "CrossLoc Benchmark Datasets Setup" tool into CSV.
-https://github.com/TOPO-EPFL/CrossLoc-Benchmark-Datasets
-CSV row output format :
-x, y, z, yaw, pitch, roll
+Input : dataset from https://github.com/TOPO-EPFL/CrossLoc-Benchmark-Datasets
+output : CSV file [x, y, z, yaw, pitch, roll]
+---
+Usage : fill the corresponding section in the config file and run this script. 
 ---
 regis.longchamp@epfl.ch
 """
@@ -18,9 +19,9 @@ from utils_angles import pose_2_sixd_array
 
 def pose_2_cvs_arrays(coordinate_system : str = 'local' ):
     """
-    Convert positions and angles from the pose into WGS84 and ° and write a result csv
-    :param path_poses: path or list of paths of the npy pose files
-    :return: none
+    Convert positions and angles from the pose into WGS84 lat/lng coordinates and ° 
+    Gather all poses into arrays, saved in CSV
+    param : coordinate_system = ['local','wgs84','ecef']
     """
 
     path_output_file = settings.path_file_poses_csv
@@ -48,17 +49,11 @@ def pose_2_cvs_arrays(coordinate_system : str = 'local' ):
             else :
                 raise Exception("Only 'local' (default), 'wgs84' or 'ecef' coordinate system can be provided")
 
-            # current_row.insert(0,file)
-
-
             row_csv_file.append(current_row)
 
         with open(path_output_file, "w", newline="") as f:
             writer = csv.writer(f)
             writer.writerows(row_csv_file)
-
-
-
 
 
 if __name__ == '__main__':
